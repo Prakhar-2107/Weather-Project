93b0afb53fed47b14262848dafc1e92bd8896a50
Prakhar-2107 <prakharsrivastava_se20b8_74@dtu.ac.in>
2023-07-11 00:00:24 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faFacebook, faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a>
+          Copyright &copy; 2023
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="https://www.gauravghai.dev/">
+          Prakhar Srivastava
+        </a>{" "}
+        |
+        <a href="https://www.facebook.com">
+          <FontAwesomeIcon icon = {faFacebook} />
+        </a>
+        <a href="https://www.github.com">
+          <FontAwesomeIcon icon = {faGithub} />
+        </a>
+        <a href="https://www.instagram.com">
+          <FontAwesomeIcon icon = {faInstagram} />
+        </a>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "3e94d1203b84047b064492438fbc2449",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
